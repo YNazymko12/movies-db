b85bba350ed7fbe0e3a365e087335d2478b74b77
YNazymko12 <yulychka12@gmail.com>
2024-04-05 11:32:22 +0200

    lesson-1

---
+++ src/features/Movies/Movies.tsx
@@ -0,0 +1,34 @@
+import { connect } from 'react-redux';
+import { RootState } from '../../store';
+import { Movie } from '../../reducers/movies';
+import { MovieCard } from './MovieCard';
+
+import './Movies.css';
+
+interface Props {
+  movies: Movie[];
+}
+
+function Movies({ movies }: Props) {
+  return (
+    <section>
+      <div className="Movies-list">
+        {movies.map(m => (
+          <MovieCard
+            key={m.id}
+            id={m.id}
+            title={m.title}
+            overview={m.overview}
+            popularity={m.popularity}
+          />
+        ))}
+      </div>
+    </section>
+  );
+}
+
+const mapStateToProps = (state: RootState) => ({ movies: state.movies.top });
+
+const connector = connect(mapStateToProps);
+
+export default connector(Movies);
